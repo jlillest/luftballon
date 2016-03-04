8c9e790538f77454da5442f8b14858576714241c
Jon Lillestolen <jlillest@gmail.com>
2016-03-03 23:44:05 -0500

    Add interrupt library, add example

--- src/luftballon.ino
+++ src/luftballon.ino
@@ -3,21 +3,26 @@ Luftballon
  - must see everything
 *******************************************************************************/
 #include <Adafruit_MotorShield.h>
+#include <TimerOne.h>
 
 #define LED_PIN 13
+
+bool led_state = LOW;
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // setup motor here - https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino?view=all
 
 void setup() {
+  Timer1.initialize(1000000);
+  Timer1.attachInterrupt(secondTimer);
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(1);           
-  digitalWrite(LED_PIN, LOW); 
-  delay(999);           
 }
 
+void secondTimer() {
+  led_state = !led_state;
+  digitalWrite(LED_PIN, led_state);
+}
